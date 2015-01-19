9d89a3dc6b34d264d24591abc4e43072c0f5efc0
Toni Petrina <tonipetrina@hotmail.com>
2015-01-19 15:22:22 +0100

    Added ViewModelLocatorBase and helper methods for SimpleIoc registrations

---
+++ MassivePixel.Mobile.Common/Mvvm/SimpleIocExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace MassivePixel.Common.Mvvm
+{
+    public static class SimpleIocExtensions
+    {
+        public static void TryRegister<TClass>(this SimpleIoc simpleIoc)
+            where TClass : class
+        {
+            if (simpleIoc == null)
+                throw new ArgumentNullException();
+
+            if (!simpleIoc.IsRegistered<TClass>())
+                simpleIoc.Register<TClass>();
+        }
+
+        public static void TryRegister<TInterface, TClass>(this SimpleIoc simpleIoc)
+            where TInterface : class
+            where TClass : class, TInterface
+        {
+            if (!simpleIoc.IsRegistered<TInterface>())
+                simpleIoc.Register<TInterface, TClass>();
+        }
+
+        public static void TryRegister<TClass>(this SimpleIoc simpleIoc, TClass instance)
+            where TClass : class
+        {
+            if (!simpleIoc.IsRegistered<TClass>())
+                simpleIoc.Register(() => instance);
+        }
+    }
+}
---
+++ MassivePixel.Mobile.Common/Mvvm/ViewModelLocatorBase.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace MassivePixel.Common.Mvvm
+{
+    public class ViewModelLocatorBase<TLocator>
+        where TLocator : ViewModelLocatorBase<TLocator>, new()
+    {
+        #region Singleton instance
+        private static TLocator _instance;
+
+        public static TLocator Instance
+        {
+            get { return _instance = _instance ?? new TLocator(); }
+        }
+        #endregion
+
+        public ViewModelLocatorBase()
+        {
+            // When using WP this instance might be created in App.xaml
+            _instance = _instance ?? this as TLocator;
+
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+        }
+    }
+}
